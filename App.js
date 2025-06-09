af7814df596ed7c19f9facd76d01e445952e9a36
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-06-09 21:18:38 +0800

    working with combined styles andlearn how style precedence works

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ export default function App() {
         <Text style={styles.dummyText}>My name is</Text>
       </View>
       <Text style={styles.dummyText}>Lance Kian Flores</Text>
+      <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
+      <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
       <Button title="Tap me" />
     </View>
   );
@@ -25,5 +27,11 @@ const styles = StyleSheet.create({
     padding: 16,
     borderWidth: 2,
     borderColor: "blue",
+  },
+  bigBlue: {
+    color: "blue",
+  },
+  red: {
+    color: "red",
   },
 });

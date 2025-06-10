bde8b54b70868df464b6beb69ec844e35e41fa15
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-06-10 22:11:24 +0800

    playing around flexbox in react native

--- App.js
+++ App.js
@@ -12,15 +12,58 @@ export default function App() {
     // <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
     //       <Button title="Tap me" />
     //     </View>
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal" />
-        <Button title="Add goal" />
+    <View
+      style={{
+        padding: 50,
+        flexDirection: "row",
+        width: "80%",
+        height: 300,
+        justifyContent: "space-around",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "red",
+
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>1</Text>
       </View>
-      <View>
-        <Text>List of goals...</Text>
+
+      <View
+        style={{
+          backgroundColor: "blue",
+          flex: 2,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "green",
+
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>3</Text>
       </View>
     </View>
+    // <View style={styles.appContainer}>
+    //   <View style={styles.inputContainer}>
+    //     <TextInput style={styles.textInput} placeholder="Your course goal" />
+    //     <Button title="Add goal" />
+    //   </View>
+    //   <View>
+    //     <Text>List of goals...</Text>
+    //   </View>
+    // </View>
   );
 }
 

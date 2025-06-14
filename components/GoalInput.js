879ec02d97e820e2ff9a87ba0df4e112ca94c7f0
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-06-14 23:45:33 +0800

    Include the import and use props

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -9,17 +9,4 @@ const GoalInput = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc",
-    color: "white",
-  },
-  goalText: {
-    color: "white",
-  },
-});
-
 export default GoalInput;
